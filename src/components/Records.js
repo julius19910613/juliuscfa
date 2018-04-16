d3045902c1aa21443e85c67348309752522cf838
julius19910613 <lifanchenjulius@hotmail.com>
2018-04-16 21:29:04 +0800

    list function 2

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,56 +1,72 @@
 import React, { Component } from 'react';
 import Record from './Record';
+import axios from 'axios';
+import * as RecordsAPI from '../utils/RecordsAPI'
 
 class Records extends Component {
   constructor() {
     super();
     this.state = {
-      records: [
-        {
-          "id": 1,
-          "date": "2017-03-17",
-          "title": "income",
-          "amount": 20
-        },
-        {
-          "id": 2,
-          "date": "2017-03-17",
-          "title": "outcome",
-          "amount": 20
-        },
-        {
-          "id": 3,
-          "date": "2017-03-17",
-          "title": "income",
-          "amount": 20
-        }
-      ]
+      error: null,
+      isLoaded: false,
+      records: []
     }
+  }
+
+  componentDidMount() {
+    axios.get("localhost:3004/records").then(
+      response => {
+        this.setState({
+          records: response.data,
+          isLoaded: true
+        })
+      }
+    )
+    .catch(
+      (error)=> {
+        this.setState({
+          records: [],
+          isLoaded: true
+        }
+
+        )
+      }
+
+    )
+
+
   }
 
 
   render() {
 
+    const { error, isLoaded, records } = this.state;
 
-
-    return (
-      <div>
-        <h2>Records</h2>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <td>Date</td>
-              <td>Time</td>
-              <td>Amount</td>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.records.map((record) => <Record record={record} />)}
-          </tbody>
-        </table>
-
-      </div>
-    );
+    if(error){
+      return <div>Error: {error.message}</div>;
+    }else if (!isLoaded) {
+      return <div>loading...</div>;
+    }else {
+      return (
+        <div>
+          <h2>Records</h2>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <td>Date</td>
+                <td>Time</td>
+                <td>Amount</td>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.records.map((record) =>
+                <Record key={record.id} {...record}  />)}
+            </tbody>
+          </table>
+  
+        </div>
+      );
+    }
   }
 }
 

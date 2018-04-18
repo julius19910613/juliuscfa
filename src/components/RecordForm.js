33591f45dd3c0f4a762d5675d2218eefd3c11bad
Julius Li <julius.li@sap.com>
2018-04-18 18:52:05 +0800

    create record ui

--- src/components/RecordForm.js
+++ src/components/RecordForm.js
@@ -28,16 +28,20 @@ export default class RecordForm extends Component {
         return this.state.date && this.state.title && this.state.amount;
     }
 
+    handleSubmit(event){
+        event.preventDefault();
+    }
+
   render() {
     return (
-      <form className="form-inline">
-      <div className="form-group">
+      <form className="form-inline mb-3" onSubmit = {this.handleSubmit.bind(this)}>
+      <div className="form-group mr-1">
         <input type="text" className="form-control" onChange = {this.handleChange.bind(this)}placeholder="Date" name = "date"/> 
       </div>
-      <div className="form-group">
+      <div className="form-group mr-1">
         <input type="text" className="form-control" onChange = {this.handleChange.bind(this)} placeholder="Title" name = "title"/> 
       </div>
-      <div className="form-group">
+      <div className="form-group mr-1">
         <input type="text" className="form-control" onChange = {this.handleChange.bind(this)} placeholder="Amount" name = "amount"/> 
       </div>
         <button type = "submit" disabled = {!this.valid()} className="btn btn-primary">Create Record</button>
